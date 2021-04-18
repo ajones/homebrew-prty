3378ceb4e0617b054082677b7a2ef153d72dade9
Aaron Jones <r.aaron.jones@gmail.com>
2021-04-18 11:18:41 -0700

    updating sha to match

--- formula/prty.rb
+++ formula/prty.rb
@@ -4,7 +4,7 @@ class PRTY < Formula
   desc "Intellegent PR Review"
   homepage "https://github.com/ajones/prty"
   url "https://github.com/ajones/homebrew-prty/archive/refs/tags/v0.0.1.tar.gz",
-  sha256 "c4bb34abd18ef743572c085fcbaf0a4d5c0770a7c889ae5ce422213bb35e80d4"
+  sha256 "f81ecef0cb290c646e1d5bec1abf5f7db666ffe903b2b1cd6eb584356e1a7cee"
   head "https://github.com/ajones/prty.git"
 
   def install
