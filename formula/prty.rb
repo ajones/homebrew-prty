aa6537f64b64b0866a8271131e77324b3b10e8cb
Aaron Jones <r.aaron.jones@gmail.com>
2021-04-18 21:46:35 -0700

    updating to new shasum

--- formula/prty.rb
+++ formula/prty.rb
@@ -4,7 +4,7 @@ class Prty < Formula
   desc "Intellegent PR Review"
   homepage "https://github.com/ajones/prty"
   url "https://github.com/ajones/homebrew-prty/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "f81ecef0cb290c646e1d5bec1abf5f7db666ffe903b2b1cd6eb584356e1a7cee"
+  sha256 "fa8ff1ac5e02f9221810111c56121e9da8529d309d403eaeb39a46bac06d6890"
   head "https://github.com/ajones/prty.git"
 
   def install
