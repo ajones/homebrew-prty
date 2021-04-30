42bf4ec15fc867a8a2bb0ab33766747cca67bcf2
Aaron Jones <r.aaron.jones@gmail.com>
2021-04-29 20:00:05 -0700

    v0.0.8 release

--- formula/prty.rb
+++ formula/prty.rb
@@ -3,8 +3,8 @@ require "formula"
 class Prty < Formula
   desc "Intellegent PR Review"
   homepage "https://github.com/ajones/prty"
-  url "https://github.com/ajones/homebrew-prty/archive/refs/tags/v0.0.7.tar.gz"
-  sha256 "4586a415b363d5786885b9f2df873079bab5827b759d891485bbe66d3bad85c4"
+  url "https://github.com/ajones/homebrew-prty/archive/refs/tags/v0.0.8.tar.gz"
+  sha256 "9862e8d329bb69c10f93d3be8eb2ceeedcd1780ecdea113540a53d6c4f74c3ee"
   head "https://github.com/ajones/prty.git"
 
   def install
@@ -13,6 +13,6 @@ class Prty < Formula
 
   # Homebrew requires tests.
   test do
-    assert_match "version 0.0.7", shell_output("#{bin}/prty -v", 2)
+    assert_match "version 0.0.8", shell_output("#{bin}/prty -v", 2)
   end
 end
